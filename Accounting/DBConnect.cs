dabd2d8dae39a9cb0a4b3d578b3e7954e6f52a62
Михаил Мясников <gutufik@mail.ru>
2021-12-13 09:23:41 +0300

    Add project files.

---
+++ Accounting/DBConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting
+{
+    public class DBConnect
+    {
+        public static AccountingEntities connection = new AccountingEntities();
+    }
+}
